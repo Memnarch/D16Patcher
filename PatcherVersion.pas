2ca4225d2d4544a5c1bab5c5c618d92253681730
Memnarch <Sebal007@freenet.de>
2013-04-15 22:16:41 +0200

    modified Version File

--- PatcherVersion.pas
+++ PatcherVersion.pas
@@ -4,9 +4,9 @@ interface
 
 const
   CMajorPatcherVersion = '1';
-  CMinorPatcherVersion = '1';
+  CMinorPatcherVersion = '0';
   CReleasePatcherVersion = '0';
-  CPatcherVersion = CMajorPatcherVersion + '.' + CMinorPatcherVersion + '.' + CReleasePatcherVersion;
+  CPatcherVersion: string = CMajorPatcherVersion + '.' + CMinorPatcherVersion + '.' + CReleasePatcherVersion;
   CPatcherName = 'D16Patcher';
 
 implementation
